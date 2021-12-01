7a4a9e260dc6803840c0a1f6190d4f033df5b224
Nicolas <n_ahn2@u.pacific.edu>
2021-12-01 01:10:59 -0800

    Created Perceptron class + output method

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self, input_num, output_num):
+        self.weights = []
+        self.input_num = input_num
+        self.output_num = output_num
+
+    def output(self, x, weights):
+        z = np.dot(x, weights)
+        if z>0.5: return 1
+        else: return 0
 
 if __name__ == '__main__':
     print("Hello World!")
