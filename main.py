0fc2bd85bad1e87746bc01e1b5e3e59a3a4674e2
Nicolas <n_ahn2@u.pacific.edu>
2021-12-01 01:14:54 -0800

    Created predict() method to get actual outputs

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ class Perceptron:
         self.input_num = input_num
         self.output_num = output_num
 
+    # Creates the output of a single Neuron of the Perceptron
     def output(self, x, weights):
         z = np.dot(x, weights)
         if z>0.5: return 1
         else: return 0
+
+    # function to get Actual_Output given inputs x
+    # input: list:x -> list that contains inputs (longitude, latitude)
+    def predict(self, x):
+        actual = []
+        for i in range(self.output_num)
+            actual.append(self.output(x, self.weights[i]))
+        # for w in weights:
+        #     actual.append(output(x, w))
+
+        return actual
 
 if __name__ == '__main__':
     print("Hello World!")
